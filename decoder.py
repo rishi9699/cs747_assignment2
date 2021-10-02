616d51cdca0e9b5996567168c5a803fa51f3859e
rishi9699 <rishi9699@gmail.com>
2021-10-02 11:37:07 +0530

    decoder done (hopefully)

--- decoder.py
+++ decoder.py
@@ -1,12 +1,23 @@
-f = open('./pa2_base/pa2_base/data/attt/states/states_file_p2.txt')
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--value-policy")
+parser.add_argument("--states")
+parser.add_argument("--player-id")
+
+args = parser.parse_args()
+
+f = open(args.states)
 states = f.read().split()
 states = dict(zip(range(len(states)), states))
 
-f = open('./p2optimalpolicy.txt')
-print(1)
-i=0
+
+print(args.player_id)
+
+f = open(args.value_policy)
 line = f.readline().split()
 actions = [0]*9
+i=0
 while len(line)!=0:
     print(states[i], end=' ')
     actions[int(line[1])]=1
